ad59940e555c644c4afc87aca7dcb951419ba37e
allarsen <alexander.larsen@keysight.com>
2022-01-25 17:21:07 +0100

    Add elaborating comment to file open options

--- Package/PackageInstallHelpers/FileLocks.cs
+++ Package/PackageInstallHelpers/FileLocks.cs
@@ -5,19 +5,24 @@ using System.Threading;
 
 namespace OpenTap.Package.PackageInstallHelpers
 {
-    /// <summary> Locks a file using flock on linux. This essentially works as a mutex.  </summary>
+    /// <summary> Locks a file using flock on linux. This essentially works as a named mutex.  </summary>
     class PosixFileLock : IDisposable
     {
         int fd;
         public static PosixFileLock Take(string file)
         {
-            int fd = PosixNative.open(file, PosixNative.O_RDWR | PosixNative.O_APPEND,
-                Convert.ToInt32("666", 8));
+            // Open 'file' in read/write + append mode. If the file does not exist it will be created with the
+            // most permissive access settings possible
+            int fd = PosixNative.open(file, PosixNative.O_RDWR | PosixNative.O_APPEND, PosixNative.ALL_READ_WRITE);
             var lockObj = new PosixFileLock() {fd = fd};
             lockObj.Take();
             return lockObj;
         }
 
+        /// <summary>
+        /// Request an exclusive lock on the open file handle
+        /// This call wil block until the lock is acquired
+        /// </summary>
         void Take() => PosixNative.flock(fd, PosixNative.LOCK_EX);
         void Release() => PosixNative.close(fd);
         public void Dispose()
@@ -66,5 +71,16 @@ namespace OpenTap.Package.PackageInstallHelpers
         public const int O_RDWR = 2; //00000002;
         public const int LOCK_SH = 1;
         public const int LOCK_EX = 2;
+
+        public const int S_IRUSR = 256; //00000400
+        public const int S_IWUSR = 128; //00000200
+
+        public const int S_IRGRP = 32;  //00000040
+        public const int S_IWGRP = 16;  //00000020
+
+        public const int S_IROTH = 4;   //00000004
+        public const int S_IWOTH = 2;   //00000002
+
+        public const int ALL_READ_WRITE = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
     }
 }
